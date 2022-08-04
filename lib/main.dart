a79b4ee8b76fcd7344986d45fed6a15a3e255214
Alan Barnett <alanbar@clear.net.nz>
2022-08-04 15:58:07 +1200

    Tom Smith

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _NinjaCardState extends State<NinjaCard> {
             ),
             const SizedBox(height: 10.0),
             Text(
-              'Tom Jones',
+              'Tom Smith',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 fontWeight: FontWeight.bold,
